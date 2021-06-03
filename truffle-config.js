c5d4df0076e3d84a6e3edcb0b3c53cb210ddf2d5
Umut Yerli <umutyerli@Mittys-MacBook.local>
2021-06-03 17:59:07 -0500

    added kovan network to truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -67,33 +67,16 @@ module.exports = {
           //url to ethereum node
           endpointUrl
         )
-      }
-    },
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
+    }
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
-  },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
-
-  // Configure your compilers
-  compilers: {
-    solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
-    },
-  },
+  }
 };
